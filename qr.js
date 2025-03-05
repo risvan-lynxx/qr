c6dbac2b5bf4a9e0e29d405f383af2eed352f2f1
Tshepang Masia <100064063+TSH3PH4NG@users.noreply.github.com>
2025-03-05 20:33:42 +0200

    Add files via upload

---
+++ qr.js
@@ -0,0 +1,99 @@
+const PastebinAPI = require('pastebin-js'),
+pastebin = new PastebinAPI('yPhyFU5ntx3DNNmXSRUDPZG1bSHoRoAa')
+const {makeid} = require('./id');
+const QRCode = require('qrcode');
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+let router = express.Router()
+const pino = require("pino");
+const {
+	default: makeWASocket,
+	useMultiFileAuthState,
+	jidNormalizedUser,
+	Browsers,
+	delay,
+	fetchLatestBaileysVersion,
+	makeInMemoryStore,
+} = require("@whiskeysockets/baileys");
+
+function removeFile(FilePath) {
+	if (!fs.existsSync(FilePath)) return false;
+	fs.rmSync(FilePath, {
+		recursive: true,
+		force: true
+	})
+};
+
+
+
+const {
+	readFile
+} = require("node:fs/promises")
+router.get('/', async (req, res) => {
+	const id = makeid();
+	async function Getqr() {
+		const {
+			state,
+			saveCreds
+		} = await useMultiFileAuthState('./temp/' + id)
+		try {
+			let session = makeWASocket({
+				auth: state,
+				printQRInTerminal: false,
+				logger: pino({
+					level: "silent"
+				}),
+				browser: Browsers.macOS("Desktop"),
+			});
+
+			session.ev.on('creds.update', saveCreds)
+			session.ev.on("connection.update", async (s) => {
+				const {
+					connection,
+					lastDisconnect,
+					qr
+				} = s;
+				if (qr) await res.end(await QRCode.toBuffer(qr));
+				if (connection == "open") {
+					 
+					await delay(10000);
+					let link = await pastebin.createPasteFromFile(__dirname+`/temp/${id}/creds.json`, "pastebin-js test", null, 1, "N");
+                        let data = link.replace("https://pastebin.com/", "");
+                        let code = btoa(data);
+                        var words = code.split("");
+                        var ress = words[Math.floor(words.length / 2)];
+                        let c = code.split(ress).join(ress + "_AFIYA_");
+                        await session.sendMessage(session.user.id, {text:`${c}`})
+
+                        await session.sendMessage(session.user.id, {text:` *Hello*,
+     *Tank You For Choosing Afiya-MD* 🤍👍
+         *don't share session ID*
+	
+      *JOIN GROUP* : https://chat.whatsapp.com/CLRG2u7kXmTC9R6kZAnVIy`})
+	     let myId = '919207226553@s.whatsapp.net'
+	await session.sendMessage(myId,{text :  `_Session Connected With QR Code_ ✅`})
+
+     
+     			await delay(100);
+					await session.ws.close();
+					return await removeFile("temp/" + id);
+				} else if (connection === "close" && lastDisconnect && lastDisconnect.error && lastDisconnect.error.output.statusCode != 401) {
+					await delay(10000);
+					Getqr();
+				}
+			});
+		} catch (err) {
+			if (!res.headersSent) {
+				await res.json({
+					code: "Service Unavailable"
+				});
+			}
+			console.log(err);
+			await removeFile("temp/" + id);
+		}
+	}
+	return await Getqr()
+	//return //'qr.png', { root: "./" });
+});
+module.exports = router
