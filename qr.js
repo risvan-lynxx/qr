75e7895bbeb170f801057bc099ac1e8acb177b93
Lynx <risvanlynx777@gmail.com>
2025-05-08 20:23:21 +0530

    Update qr.js

--- qr.js
+++ qr.js
@@ -1,86 +1,77 @@
-const { upload } = require("./upload");
-const {makeid} = require('./id');
+const axios = require('axios');
+const { create } = require('./session');
+const { makeid } = require('./id');
 const QRCode = require('qrcode');
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
-let router = express.Router()
 const pino = require("pino");
 const {
-	default: makeWASocket,
-	useMultiFileAuthState,
-	jidNormalizedUser,
-	Browsers,
-	delay,
-	fetchLatestBaileysVersion,
-	makeInMemoryStore,
+    default: makeWASocket,
+    useMultiFileAuthState,
+    jidNormalizedUser,
+    Browsers,
+    delay,
+    makeInMemoryStore,
 } = require("@whiskeysockets/baileys");
 
+const { readFile } = require("node:fs/promises")
+
+let router = express.Router()
+
 function removeFile(FilePath) {
-	if (!fs.existsSync(FilePath)) return false;
-	fs.rmSync(FilePath, {
-		recursive: true,
-		force: true
-	})
+    if (!fs.existsSync(FilePath)) return false;
+    fs.rmSync(FilePath, {
+        recursive: true,
+        force: true
+    })
 };
 
-
-
-const {
-	readFile
-} = require("node:fs/promises")
 router.get('/', async (req, res) => {
-	const id = makeid();
-	async function Getqr() {
-		const {
-			state,
-			saveCreds
-		} = await useMultiFileAuthState('./temp/' + id)
-		try {
-			let session = makeWASocket({
-				auth: state,
-				printQRInTerminal: false,
-				logger: pino({
-					level: "silent"
-				}),
-				browser: Browsers.macOS("Desktop"),
-			});
+    const id = makeid();
+    async function Getqr() {
+        const { state, saveCreds } = await useMultiFileAuthState('./temp/' + id)
+        try {
+            let session = makeWASocket({
+                auth: state,
+                printQRInTerminal: false,
+                logger: pino({
+                    level: "silent"
+                }),
+                browser: Browsers.macOS("Desktop"),
+            });
 
-			session.ev.on('creds.update', saveCreds)
-			session.ev.on("connection.update", async (s) => {
-				const {
-					connection,
-					lastDisconnect,
-					qr
-				} = s;
-				if (qr) await res.end(await QRCode.toBuffer(qr));
-				if (connection == "open") {
-					 
-					 await delay(10000);
-					 let link = await upload(`${id}.json`,__dirname+`/temp/${id}/creds.json`);
-	                                 let code = link.split("/")[4]
-                                         await session.sendMessage(session.user.id, {text:`${code}`})
-                        
-     
-     			                await delay(100);
-					await session.ws.close();
-					return await removeFile("temp/" + id);
-				} else if (connection === "close" && lastDisconnect && lastDisconnect.error && lastDisconnect.error.output.statusCode != 401) {
-					await delay(10000);
-					Getqr();
-				}
-			});
-		} catch (err) {
-			if (!res.headersSent) {
-				await res.json({
-					code: "Service Unavailable"
-				});
-			}
-			console.log(err);
-			await removeFile("temp/" + id);
-		}
-	}
-	return await Getqr()
-	//return //'qr.png', { root: "./" });
+            session.ev.on('creds.update', saveCreds)
+            session.ev.on("connection.update", async (s) => {
+                const { connection, lastDisconnect, qr } = s;
+                if (qr) await res.end(await QRCode.toBuffer(qr));
+                if (connection == "open") {
+                    await delay(5000);
+                    await delay(5000);
+
+                    const jsonData = await fs.promises.readFile(`${__dirname}/temp/${id}/creds.json`, 'utf-8');
+                    const { id: data } = await create(jsonData);
+                    await session.sendMessage(session.user.id, { text: 'zen~' + data });
+
+                    await delay(100);
+                    await session.ws.close();
+                    return await removeFile("temp/" + id);
+                } else if (connection === "close" && lastDisconnect && lastDisconnect.error && lastDisconnect.error.output.statusCode != 401) {
+                    await delay(10000);
+                    Getqr();
+                }
+            });
+        } catch (err) {
+            if (!res.headersSent) {
+                await res.json({
+                    code: "Service Unavailable"
+                });
+            }
+            console.log(err);
+            await removeFile("temp/" + id);
+        }
+    }
+    return await Getqr()
 });
-module.exports = router
+
+module.exports = router;
